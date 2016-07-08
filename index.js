dfefe76f98d45d156fa37aac337dc985f795ae34
cloes <375600284@qq.com>
2016-07-08 23:59:55 +0800

    add index module

---
+++ index.js
@@ -0,0 +1,27 @@
+const electron = require('electron');
+
+const app = electron.app;
+
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({width:800,height:600});
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed',function(){
+        mainWindow = null;
+    });
+}
+
+app.on('ready',createWindow);
+
+app.on('window-all-closed',function(){
+  if (mainWindow === null) {
+    createWindow()
+  }
+});
