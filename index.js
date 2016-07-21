2cb596ab1373928b38cc3da2a47c6672cbccd413
cloes <375600284@qq.com>
2016-07-22 00:37:05 +0800

    add featur:添加了访问微信服务器，获取uuid

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = electron.app;
 
 const BrowserWindow = electron.BrowserWindow;
 
+const https = require('https');
+
 let mainWindow;
 
 function createWindow() {
+
     mainWindow = new BrowserWindow({width:800,height:600});
 
     mainWindow.loadURL(`file://${__dirname}/index.html`);
@@ -18,7 +21,55 @@ function createWindow() {
     });
 }
 
-app.on('ready',createWindow);
+
+function convertArrayToString(params) {
+    var output = '?';
+    for (var key in params) {
+        output += key + '=' + params[key] + '&';
+    }
+    return output.substring(0,output.length - 1);
+}
+
+function getUuid() {
+    //var url = 'https://login.weixin.qq.com/jslogin';
+    var url = 'login.weixin.qq.com';
+    var timestamp = new Date().getTime();
+    var random = timestamp + Math.floor(Math.random() * 1000);
+
+    var params = {
+      'appid': 'wx782c26e4c19acffb',
+      'fun': 'new',
+      'lang': 'zh_CN',
+      '_': random,
+    }
+    var paramsString = convertArrayToString(params);
+    var options = {
+        hostname: url,
+        path: '/jslogin' + paramsString,
+        method: 'GET',
+    };
+
+    var req = https.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        //console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        console.log('headers: ', res.headers);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+          console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+          console.log('No more data in response.')
+        })
+    });
+    req.end();
+}
+
+//app.on('ready',createWindow);
+
+app.on('ready',()=>{
+  createWindow();
+  getUuid();
+})
 
 app.on('window-all-closed',function(){
   if (mainWindow === null) {
