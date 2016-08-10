ef08acb793f8c8ef9600ccde6209f3c0b8d62191
cloes <375600284@qq.com>
2016-08-10 18:07:47 +0800

    添加扫二维码的网络通信模块

--- index.js
+++ index.js
@@ -56,12 +56,61 @@ function getUuid() {
         res.setEncoding('utf8');
         res.on('data', (chunk) => {
           console.log(`BODY: ${chunk}`);
+          var pattern = /window.QRLogin.code = (\d+); window.QRLogin.uuid = "(\S+?)"/;
+          pattern.test(chunk);
+          var uuid = RegExp.$2;
+          wait4login(uuid);
         });
         res.on('end', () => {
-          console.log('No more data in response.')
+          console.log('No more data in response.');
         })
     });
     req.end();
+}
+
+function createQRimage(){
+    var qr = require('qr-image');
+    var qr_png = qr.image('I love QR!', { type: 'png' });
+    qr_png.pipe(require('fs').createWriteStream('i_love_qr.png'));
+}
+
+function wait4login(uuid){
+    //var LOGIN_TEMPLATE = 'https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login?tip=%s&uuid=%s&_=%s';
+    var login_url = 'login.weixin.qq.com';
+    var tip = 1;
+    var try_later_secs = 1;
+    var MAX_RETRY_TIMES = 10;
+    var code = 0;
+
+    var retry_time = MAX_RETRY_TIMES;
+
+    var params = {
+      'tip': tip,
+      'uuid': uuid,
+      '_': Math.round(new Date().getTime()/1000),
+    }
+    var paramsString = convertArrayToString(params);
+    var options = {
+        hostname: login_url,
+        path: '/cgi-bin/mmwebwx-bin/login' + paramsString,
+        method: 'GET',
+    };
+
+    var req = https.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        //console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        //console.log('headers: ', res.headers);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+          console.log('2headers: ', res.headers);
+          console.log(`2BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+          console.log('2No more data in response.');
+        })
+    });
+    req.end();
+
 }
 
 //app.on('ready',createWindow);
@@ -69,6 +118,7 @@ function getUuid() {
 app.on('ready',()=>{
   createWindow();
   getUuid();
+  createQRimage();
 })
 
 app.on('window-all-closed',function(){
